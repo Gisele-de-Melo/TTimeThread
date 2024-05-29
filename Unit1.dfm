484da0b3b6b9c1cc2c4dbf944e26e03f09927d5d
Gisele de Melo <gisele_demelo@hotmail.com>
2024-05-29 11:16:54 -0300

    TTimeThread blog codedelphi

---
+++ Unit1.dfm
@@ -0,0 +1,31 @@
+object Form1: TForm1
+  Left = 0
+  Top = 0
+  Caption = 'Form1'
+  ClientHeight = 161
+  ClientWidth = 334
+  Color = clBtnFace
+  Font.Charset = DEFAULT_CHARSET
+  Font.Color = clWindowText
+  Font.Height = -12
+  Font.Name = 'Segoe UI'
+  Font.Style = []
+  Position = poScreenCenter
+  TextHeight = 15
+  object Label1: TLabel
+    Left = 24
+    Top = 16
+    Width = 138
+    Height = 15
+    Caption = 'Tempo decorrido: 00:00:00'
+  end
+  object Button1: TButton
+    Left = 24
+    Top = 56
+    Width = 75
+    Height = 25
+    Caption = 'Button1'
+    TabOrder = 0
+    OnClick = Button1Click
+  end
+end
---
+++ Unit1.pas
@@ -0,0 +1,86 @@
+unit Unit1;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls, System.DateUtils;
+
+type
+  TTimeThread = class(TThread)
+  private
+    FInterval: Integer;
+    FOnTimeElapsed: TThreadProcedure;
+  protected
+    procedure Execute; override;
+  public
+    constructor Create(Interval: Integer; OnTimeElapsed: TThreadProcedure);
+  end;
+
+  TForm1 = class(TForm)
+    Button1: TButton;
+    Label1: TLabel;
+    procedure Button1Click(Sender: TObject);
+    procedure FormClose(Sender: TObject; var Action: TCloseAction);
+  private
+    TimeThread: TTimeThread;
+    StartTime: TDateTime;
+    procedure OnTimeElapsed;
+  public
+    { Public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.dfm}
+
+{ TTimeThread }
+
+constructor TTimeThread.Create(Interval: Integer; OnTimeElapsed: TThreadProcedure);
+begin
+  inherited Create(False);
+  FreeOnTerminate := True;
+  FInterval := Interval;
+  FOnTimeElapsed := OnTimeElapsed;
+end;
+
+procedure TTimeThread.Execute;
+begin
+  while not Terminated do
+  begin
+    Sleep(FInterval);
+    Synchronize(FOnTimeElapsed);
+  end;
+end;
+
+{ TForm1 }
+
+procedure TForm1.Button1Click(Sender: TObject);
+begin
+  // Inicia a thread para controlar o tempo
+  StartTime := Now;
+  Label1.Caption := 'Tempo decorrido: 0 segundos';
+  TimeThread := TTimeThread.Create(1000, OnTimeElapsed);
+end;
+
+procedure TForm1.FormClose(Sender: TObject; var Action: TCloseAction);
+begin
+  // Certifica-se de finalizar a thread quando o formulário é fechado
+  if Assigned(TimeThread) then
+    TimeThread.Terminate;
+end;
+
+procedure TForm1.OnTimeElapsed;
+var
+  ElapsedTime: TDateTime;
+begin
+  // Calcula o tempo decorrido desde o início
+  ElapsedTime := Now - StartTime;
+  // Atualiza o rótulo com o tempo decorrido formatado
+  Label1.Caption := 'Tempo decorrido: ' + FormatDateTime('hh:nn:ss', ElapsedTime);
+end;
+
+end.
